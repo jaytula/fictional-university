fcbec19bce57769bd9153d6c763faf42a2e26ae3
Jay T <jaytula@gmail.com>
2021-09-17 15:57:44 -0700

    17. Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -14,6 +14,7 @@ while (have_posts()) {
 
   <div class="container container--narrow page-section">
     <?php $parent_id = wp_get_post_parent_id(get_the_ID()); ?>
+    <?php $child_of = $parent_id ? $parent_id : get_the_ID(); ?>
     <?php if($parent_id): ?>
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
@@ -22,14 +23,25 @@ while (have_posts()) {
     </div>
     <?php endif; ?>
 
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    <?php
+    $test_array = get_pages(array(
+      'child_of' => get_the_id(),
+    ));
+    ?>
+    <?php if($parent_id || $test_array): ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?= get_permalink($parent_id) ?>"><?= get_the_title($parent_id) ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php 
+          wp_list_pages(array(
+            'title_li' => null,
+            'child_of' => $child_of,
+            'sort_column' => 'menu_order'
+          ));
+        ?>
       </ul>
-    </div> -->
-
+    </div>
+    <?php endif; ?>
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
