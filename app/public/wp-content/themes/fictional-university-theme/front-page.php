9c9b3b933e3f87c5ff7dc41e1fd011c8f1983a94
Jay T <jaytula@gmail.com>
2021-09-22 00:39:59 -0700

    25. Blog: Few Quick Edits & Improvements

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -64,7 +64,7 @@
       <?php wp_reset_postdata(); ?>
 
 
-      <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+      <p class="t-center no-margin"><a href="#" class="btn btn--yellow"><?= site_url('/blog') ?></a></p>
     </div>
   </div>
 </div>
