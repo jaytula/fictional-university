fdc1801dd17c576c4c4499e7c54e737622e8f40e
Jay T <jaytula@gmail.com>
2021-09-22 00:54:10 -0700

    26. Custom Post Types

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -12,6 +12,7 @@ function university_files() {
 function university_features() {
   add_theme_support('title-tag');
 }
+
 add_action('wp_enqueue_scripts', 'university_files');
 add_action('after_setup_theme', 'university_features');
 
